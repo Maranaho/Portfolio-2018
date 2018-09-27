231462fcb01c120576dcdf39380da8940283ea60
Maranaho <maranaho.nguessan@gmail.com>
2018-09-27 14:38:19 +0100

    @TODO Maybe this belongs in the state

--- src/components/Illustrations/Illustrations.js
+++ src/components/Illustrations/Illustrations.js
@@ -14,7 +14,9 @@ class Illustrations extends Component {
       pos:50,
       listLength:Images.length,
       listWidth:5000,
-      showCompactStatusBar: false
+      showCompactStatusBar: false,
+      itm1st: true,
+      itmlast: false
     }
 
     this.handleWheel = this.handleWheel.bind(this)
@@ -31,46 +33,64 @@ class Illustrations extends Component {
 
 
   handleWheel(e){
-    let s = this.state
-    let p = s.pos
-    let move = s.pos + -e.deltaY
-    if (this.state.hasScroll) {
-      if (p < 50  || e.deltaY === 100) {
-        if (move > 50) {move = 50}
-        this.setState({pos:move})
-      }
-    } else { this.setState({hasScroll:true}) }
+    let lastFewItems = 0
+    const
+    s = this.state,
+    p = s.pos,
+    listElt = document.getElementById('illusList'),
+    list = Array.from(listElt.children),
+    lgn = s.listLength,
+    vw = window.innerWidth,
+    itmOnScreen = Math.round(vw/(s.listWidth/lgn));
+    for (let i = 0; i < itmOnScreen; i++) {
+      lastFewItems = lastFewItems + list[lgn - (i+1)].clientWidth
+    }
+
+    //@TODO Maybe this belongs in the state
+
+
+    if (p >= -(s.listWidth - lastFewItems)) {
+      let move = s.pos + -e.deltaY
+      if (this.state.hasScroll) {
+        if (p < 50  || e.deltaY === 100) {
+          if (move > 50) {move = 50}
+          this.setState({pos:move})
+        }
+      } else { this.setState({hasScroll:true}) }
+    } else { this.setState({pos:-(s.listWidth - lastFewItems)}) }
   }
 
-  handleMDown(e){
-    this.setState({start:e.clientX})
-  }
+  handleMDown(e){ this.setState({start:e.clientX}) }
 
   handleMUp(e){
     let s = this.state
     let move
     if (s.start && s.pos <= 50) {
       move = (s.pos - s.start + e.clientX)
-      if (move > 50) { move = 50 }
+      console.log(move);
+      if (move >= 50) { move = 50 }
+      if (move <= 50) { move = 50 }
       this.setState({pos:move})
     }
   }
 
   handleNxtPrv(e){
-    //let move
+    let move
     const
-    nxt = e.target.id === 'nxt';
-    if (nxt) {
-      //@TODO trouver ou on est
-    }
-    //this.setState({pos:move})
+    nxt = e.target.id === 'nxt',
+    s = this.state,
+    itm = s.listWidth / s.listLength
+    if (nxt) { move = s.pos - itm } else { move = s.pos + itm }
+    this.setState({pos:move})
   }
 
   setProperWidth(){
     const s = this.state;
     if (s.listWidth === 5000) {
+      let w = 0
       let listElt = document.getElementById('illusList')
-      this.setState({ listWidth: listElt.children[0].clientWidth * s.listLength})
+      Array.from(listElt.children).map(c=> w = w + c.clientWidth)
+      this.setState({ listWidth: w})
     }
   }
 
